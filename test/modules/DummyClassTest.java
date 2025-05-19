51b8cc6e34b008df9084b4a2da3bf3ee7fb1f7bc
HimaSaiCharan <127401464+HimaSaiCharan@users.noreply.github.com>
2025-05-19 10:26:34 +0530

    created a dummy class and wrote a test

---
+++ test/modules/DummyClassTest.java
@@ -0,0 +1,12 @@
+package modules;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DummyClassTest {
+
+    @Test
+    void testSomething() {
+        assert(true);
+    }
+}
