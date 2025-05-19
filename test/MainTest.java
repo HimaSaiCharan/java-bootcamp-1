0abc7330a6461605a745383e048b0ecffa871a6f
HimaSaiCharan <127401464+HimaSaiCharan@users.noreply.github.com>
2025-05-19 10:18:22 +0530

    created Main and Test classes

---
+++ test/MainTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void testMain() {
+        assertEquals(1, 1);
+    }
+}
